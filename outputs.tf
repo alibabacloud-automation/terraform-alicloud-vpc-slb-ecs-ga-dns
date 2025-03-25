f387a5798f6b7118df750f0c563499f2bd61e547
shanye <shanye.xzq@alibaba-inc.com>
2025-03-25 14:28:14 +0800

    module: remove unused variables; examples/complete: improve the examples

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,39 @@
 output "this_ecs_id" {
-  value = alicloud_instance.default.id
+  description = "The ID of the ECS instance."
+  value       = alicloud_instance.default.id
 }
 
 output "this_rds_id" {
-  value = alicloud_db_instance.default.id
+  description = "The ID of the RDS instance."
+  value       = alicloud_db_instance.default.id
 }
 
 output "this_slb_id" {
-  value = alicloud_slb_load_balancer.default.id
+  description = "The ID of the SLB instance."
+  value       = alicloud_slb_load_balancer.default.id
 }
 
 output "this_vpc_id" {
-  value = alicloud_vpc.default.id
+  description = "The ID of the VPC."
+  value       = alicloud_vpc.default.id
 }
 
 output "this_ecs_name" {
-  value = alicloud_instance.default.instance_name
+  description = "The name of the ECS instance."
+  value       = alicloud_instance.default.instance_name
 }
 
 output "this_ga_accelerator_id" {
-  value = alicloud_ga_accelerator.default.id
+  description = "The ID of the Global Acceleration instance."
+  value       = alicloud_ga_accelerator.default.id
 }
 
 output "this_dns_group_id" {
-  value = alicloud_dns_group.default.id
+  description = "The ID of the DNS group."
+  value       = alicloud_dns_group.default.id
 }
 
 output "this_alicloud_dns_id" {
-  value = alicloud_dns.default.id
+  description = "The ID of the DNS."
+  value       = alicloud_dns.default.id
 }
